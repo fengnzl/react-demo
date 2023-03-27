622d53e4957725c76d9db5ee18e558b93a81f032
recoveryMonster <fengsongting@gmail.com>
2023-03-27 23:15:10 +0800

    use vite and add js tools

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 const Pet = (props) => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, props.name),
     React.createElement('h2', {}, props.animal),
     React.createElement('h2', {}, props.breed),
-  ])
-}
+  ]);
+};
 
 const App = () => {
   return React.createElement('div', {}, [
@@ -20,9 +22,9 @@ const App = () => {
       breed: 'Cockatiel',
     }),
     React.createElement(Pet, { name: 'Doink', animal: 'Cat', breed: 'Mix' }),
-  ])
-}
+  ]);
+};
 
-const container = document.getElementById('root')
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App))
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(React.createElement(App));
